6a2fb48a488d06182ae4fdd6ab3a6e8af209fb8f
Konfuse <konfuse@qq.com>
2020-01-07 15:56:11 +0800

    add k8s experiment

--- trajectory-flink/src/main/java/com/konfuse/Test4MyTree.java
+++ trajectory-flink/src/main/java/com/konfuse/Test4MyTree.java
@@ -17,26 +17,45 @@ import org.apache.flink.util.Collector;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @Author: Konfuse
  * @Date: 2019/12/8 14:48
  */
 public class Test4MyTree {
+    public static double lon1 = -6.8394859 * 10000000;
+    public static double lat1 = 39.2084899 * 10000000;
+    public static double lon2 = -6.7762898 * 10000000;
+    public static double lat2 = 39.3037728 * 10000000;
+
     public static void main(String[] args) throws Exception {
         ExecutionEnvironment environment = ExecutionEnvironment.getExecutionEnvironment();
+        String data_env = "file:///home/data_set/";
+//        String data_env = "C:/Users/Konfuse/Desktop/session-cluster/data_set";
 
-        DataSet<String> data = environment.readTextFile("C:/Users/Konfuse/Desktop/FlinkResearch/data_set/data_points.txt");
-        DataSet<Point> points = data.map((MapFunction<String, Point>) s -> {
-            String[] pointData = s.split(",");
-            return new Point(
-                    Long.parseLong(pointData[0]),
-                    Double.parseDouble(pointData[1]),
-                    Double.parseDouble(pointData[2])
-            );
-        });
+        Point[] list = new Point[10000];
+        int id = 0;
+        Random random = new Random();
+        for (int i = 0; i < 10000; i++) {
+            float x = random.nextFloat() * 100 + 100;
+            float y = random.nextFloat() * 100 + 100;
+            list[id] = new Point(id, x, y);
+            ++id;
+        }
 
-        PointIndex index = new IndexBuilder().createPointIndex(points, 0.3, 4, 40, 16);
+//        DataSet<String> data = environment.fromCollection(list);
+//        DataSet<Point> points = data.map((MapFunction<String, Point>) s -> {
+//            String[] pointData = s.split(",");
+//            return new Point(
+//                    Long.parseLong(pointData[0]),
+//                    Double.parseDouble(pointData[1]),
+//                    Double.parseDouble(pointData[2])
+//            );
+//        });
+        DataSet<Point> points = environment.fromElements(list);
+
+        PointIndex index = new IndexBuilder().createPointIndex(points, 0.3, 1, 40, 16);
 
 //        index.getGlobalTree().map(new MapFunction<RTree<PartitionedMBR>, Tuple3<Integer, Long, MBR>>() {
 //            @Override
@@ -72,8 +91,8 @@ public class Test4MyTree {
 //            System.out.println("partition number is: " + partitionedMBR.getPartitionNumber());
 //        }
 
-        String knnQueryPath = "C:/Users/Konfuse/Desktop/FlinkResearch/data_set/points_knn_to_query.txt";
-        knnQueryTest(index, knnQueryPath, "C:/Users/Konfuse/Desktop/FlinkResearch/data_set/points_knn_query_result_flink.txt");
+        String knnQueryPath = data_env + "points_knn_to_query.txt";
+        knnQueryPrintTest(index, knnQueryPath, data_env + "points_knn_query_result_flink.txt");
 
 //        String areaQueryPath = "C:/Users/Konfuse/Desktop/FlinkResearch/data_set/points_areas_to_query.txt";
 //        areaQueryTest(index, areaQueryPath, "C:/Users/Konfuse/Desktop/FlinkResearch/data_set/points_areas_query_result_flink.txt");
@@ -82,6 +101,36 @@ public class Test4MyTree {
 //        circleQueryTest(index, circleQueryPath, "C:/Users/Konfuse/Desktop/FlinkResearch/data_set/points_circle_query_result_flink.txt");
 
 //        environment.execute();
+    }
+
+    public static void knnQueryPrintTest(PointIndex index, String knnQueryPath, String output) throws Exception {
+        System.out.println("************************point query test*************************");
+
+        BufferedReader reader = null;
+        Random random = new Random();
+        ArrayList<Point> list = new ArrayList<>(100);
+
+        for (int i = 0; i < 100; i++) {
+//                double x = ((lat2 - lat1) * random.nextDouble() + lat1) / 10000000;
+//                double y = ((lon2 - lon1) * random.nextDouble() + lon1) / 10000000;
+            double x = (100 * random.nextDouble() + 100);
+            double y = (100 * random.nextDouble() + 100);
+            list.add(new Point(0, x, y));
+        }
+
+        for (Point point : list) {
+            index.knnQuery(point, 100)
+                    .reduceGroup(new RichGroupReduceFunction<Point, List<Long>>() {
+                        @Override
+                        public void reduce(Iterable<Point> iterable, Collector<List<Long>> collector) throws Exception {
+                            List<Long> longList = new ArrayList<>();
+                            for (Point point : iterable) {
+                                longList.add(point.getId());
+                            }
+                            collector.collect(longList);
+                        }
+                    }).print();
+        }
     }
 
     public static void knnQueryTest(PointIndex index, String knnQueryPath, String output) throws Exception {
